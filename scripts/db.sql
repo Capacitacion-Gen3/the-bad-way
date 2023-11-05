4cd364932861ae019f281ea39242973e6c2dd984
Capacitación Ultrasist <144052554+capacitacion-ultra@users.noreply.github.com>
2023-11-05 12:07:15 -0600

    Update db.sql

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,14 +1,21 @@
+DROP DATABASE IF EXISTS prueba;
 CREATE DATABASE prueba;
-use prueba;
+USE prueba;
+
 CREATE TABLE persona (
-   id int,
-   first varchar(64),
-   last varchar(64),
-   age int
+   id INT(11) NOT NULL AUTO_INCREMENT,
+   first VARCHAR(64),
+   last VARCHAR(64),
+   second VARCHAR(64),
+   age INT(11),
+   primary key(id)
 );
-INSERT INTO persona values(1,'carlos','salinas',25);
-INSERT INTO persona values(2,'ernesto','zedillo',36);
-INSERT INTO persona values(3,'vicente','fox',19);
-INSERT INTO persona values(4,'felipe','caldeon',52);
+
+INSERT INTO persona VALUES(1,'carlos','salinas','x',25);
+INSERT INTO persona VALUES(2,'ernesto','zedillo','x',36);
+INSERT INTO persona VALUES(3,'vicente','fox','x',19);
+INSERT INTO persona VALUES(4,'felipe','caldeon','x',52);
 
 SELECT * FROM persona;
+
+SELECT 1;
